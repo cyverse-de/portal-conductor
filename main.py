5c8c5854181d794c285c1a083dceedbaa8bd3952
John Wregglesworth <wregglej@cyverse.org>
2025-08-20 09:45:31 -0700

    Reordered imports, cleaned up configuration logic, removed duplicated code

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import os
 import sys
-import pprint
-import portal_ldap
-import portal_datastore
 
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from pydantic import BaseModel
 
+import portal_datastore
+import portal_ldap
 
 app = FastAPI()
 
 
 @app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(request: Request, exc: StarletteHTTPException):
-    print(exc)
-    return JSONResponse(
-        content={"detail": exc.detail}, status_code=exc.status_code
-    )
+    print(exc, file=sys.stderr)
+    return JSONResponse(content={"detail": exc.detail}, status_code=exc.status_code)
 
 
 @app.middleware("http")
@@ -26,54 +22,26 @@ async def exception_handling_middleware(request: Request, call_next):
     try:
         return await call_next(request)
     except Exception as e:
-        print(str(e))
+        print(str(e), file=sys.stderr)
         return JSONResponse(content=str(e), status_code=500)
 
 
-portal_ldap_url = "http://portal-ldap/"
-portal_ldap_env = os.environ.get("PORTAL_LDAP_URL")
-if portal_ldap_env is not None:
-    portal_ldap_url = portal_ldap_env
+portal_ldap_url = os.environ.get("PORTAL_LDAP_URL") or "http://portal-ldap/"
+portal_datastore_url = (
+    os.environ.get("PORTAL_DATASTORE_URL") or "http://portal-datastore/"
+)
+ldap_community_group = os.environ.get("LDAP_COMMUNITY_GROUP") or "community"
+ipcservices_user = os.environ.get("IPCSERVICES_USER") or "ipcservices"
+ds_admin_user = os.environ.get("DS_ADMIN_USER") or "rodsadmin"
 
-portal_datastore_url = "http://portal-datastore/"
-portal_datastore_env = os.environ.get("PORTAL_DATASTORE_URL")
-if portal_datastore_env is not None:
-    portal_datastore_url = portal_datastore_env
-
-ldap_everyone_group = os.environ.get("LDAP_EVERYONE_GROUP")
-if ldap_everyone_group is None:
+ldap_everyone_group = os.environ.get("LDAP_EVERYONE_GROUP") or ""
+if ldap_everyone_group == "":
     print("LDAP_EVERYONE_GROUP must be set", file=sys.stderr)
     sys.exit(1)
 
-ldap_community_group = "community"
-ldap_community_group_env = os.environ.get("LDAP_COMMUNITY_GROUP")
-if ldap_community_group_env is not None:
-    ldap_community_group = ldap_community_group_env
-
-ipcservices_user = "ipcservices"
-ipcservices_user_env = os.environ.get("IPCSERVICES_USER")
-if ipcservices_user_env is not None:
-    ipcservices_user = ipcservices_user_env
-
-ds_admin_user = "rodsadmin"
-ds_admin_user_env = os.environ.get("DS_ADMIN_USER")
-if ds_admin_user_env is not None:
-    ds_admin_user = ds_admin_user_env
 
 ldap_api = portal_ldap.LDAP(portal_ldap_url)
 ds_api = portal_datastore.DataStore(portal_datastore_url)
-
-
-class CreateUserRequest(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    username: str
-    user_uid: str
-    password: str
-    department: str
-    organization: str
-    title: str
 
 
 @app.get("/", status_code=200)
@@ -82,7 +50,7 @@ def greeting():
 
 
 @app.post("/users", status_code=200)
-def add_user(user: CreateUserRequest):
+def add_user(user: portal_ldap.CreateUserRequest):
     ldap_api.create_user(user)
     ldap_api.change_password(user.username, user.password)
     ldap_api.add_user_to_group(user.username, ldap_everyone_group)
